edf5b6e140ad9e7af7cdf575d402fbc12a4eba4f
Rajeev <sinharajeev070@gmail.com>
2022-03-09 15:07:37 +0530

    commit

--- src/app/blogs.service.ts
+++ src/app/blogs.service.ts
@@ -10,16 +10,49 @@ export class BlogsService {
   blogData:any = [{
     author: "Rajeev",
     content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nisi consequatur, explicabo ipsam vel, autem laborum molestiae reiciendis ullam iure ab? Accusamus rerum placeat neque quis in. Officia, atque aspernatur! Aliquam tempore nobis sunt dolorem iste totam debitis error ab perferendis nihil magnam qui repellat, dicta, maiores pariatur ullam minima?",
-    postCategory: "First",
+    postCategory: "abc",
     postDate: Date.now(),
-    title: "This is Title",
+    title: "This is Title1",
     likes: 0,
-    comments: [{
-      commentDate: "2022-03-09",
-      comment: "Loved this Blog",
-      commentBy: "Viewer1"
-    }]
+    comments: []
+  },
+  {
+    author: "John",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nisi consequatur, explicabo ipsam vel, autem laborum molestiae reiciendis ullam iure ab? Accusamus rerum placeat neque quis in. Officia, atque aspernatur! Aliquam tempore nobis sunt dolorem iste totam debitis error ab perferendis nihil magnam qui repellat, dicta, maiores pariatur ullam minima?",
+    postCategory: "abc",
+    postDate: Date.now(),
+    title: "This is Title2",
+    likes: 0,
+    comments: []
+  },
+  {
+    author: "Steve",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nisi consequatur, explicabo ipsam vel, autem laborum molestiae reiciendis ullam iure ab? Accusamus rerum placeat neque quis in. Officia, atque aspernatur! Aliquam tempore nobis sunt dolorem iste totam debitis error ab perferendis nihil magnam qui repellat, dicta, maiores pariatur ullam minima?",
+    postCategory: "abc",
+    postDate: Date.now(),
+    title: "This is Title3",
+    likes: 0,
+    comments: []
+  },
+  {
+    author: "Harvy",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nisi consequatur, explicabo ipsam vel, autem laborum molestiae reiciendis ullam iure ab? Accusamus rerum placeat neque quis in. Officia, atque aspernatur! Aliquam tempore nobis sunt dolorem iste totam debitis error ab perferendis nihil magnam qui repellat, dicta, maiores pariatur ullam minima?",
+    postCategory: "abc",
+    postDate: Date.now(),
+    title: "This is Title4",
+    likes: 0,
+    comments: []
+  },
+  {
+    author: "Davison",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nisi consequatur, explicabo ipsam vel, autem laborum molestiae reiciendis ullam iure ab? Accusamus rerum placeat neque quis in. Officia, atque aspernatur! Aliquam tempore nobis sunt dolorem iste totam debitis error ab perferendis nihil magnam qui repellat, dicta, maiores pariatur ullam minima?",
+    postCategory: "abc",
+    postDate: Date.now(),
+    title: "This is Title5",
+    likes: 0,
+    comments: []
   }
+
 ];
 
 }
